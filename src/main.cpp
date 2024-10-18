ba1e9da0eac44e37877b897c91ec2d8e5642774c
Alex Shrestha <alexshrestha1996@gmail.com>
2024-10-17 22:51:14 -0700

    added transpose

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
-#include "Eigen/Dense"
 #include <iostream>
+#include "Eigen/Dense"
+#include "matrix.h"
 
+
+void pprintMat(const Matrix& amatrix);
 
 int main()
 {
-    std::cout << "Testing if program compiles with eigen header" << std::endl;
+    
+    std::vector<std::vector<double>> mat = {
+        {1.0, 2.0, 3.0},
+        {4.0, 5.0, 6.0}
+    };
+
+    Matrix my_matrix(mat);
+
+    std::cout << "Original matrix: \n";
+
+    pprintMat(my_matrix);
+
+    Matrix result = my_matrix.transpose();
+
+
+    std::cout << "Original matrix transposed: \n";
+    pprintMat(result);
 
     return 0;
 }
+
+void pprintMat(const Matrix& amatrix)
+{
+    for (int r = 0; r < amatrix.rows(); ++r) {
+        std::cout << "[";
+
+        for (int c = 0; c < amatrix.cols(); ++c) {
+            std::cout << amatrix[r][c];
+
+            if (c != amatrix.rows() - 1) 
+                std::cout << ", ";
+        }
+
+        std::cout << "]" << std::endl;
+    }
+
+    return;
+}
+
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -11,3 +11,18 @@ int Matrix::cols() const {return data[0].size();}
 std::vector<double>& Matrix::operator[](int index) {return data[index];}
 
 const std::vector<double>& Matrix::operator[](int index) const {return data[index];}
+
+
+Matrix Matrix::transpose() const {
+    int r = rows();
+    int c = cols();
+
+    Matrix result(c, r);
+
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j)
+            result[j][i] = data[i][j];
+    }
+
+    return result;
+}
